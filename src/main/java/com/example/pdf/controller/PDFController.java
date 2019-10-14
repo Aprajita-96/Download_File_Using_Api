f9a6c27b6b4f66ffbaf062a2476fa813bef2dcf9
Aprajita-96 <aprajithabajpai@gmail.com>
2019-10-14 16:33:13 +0530

    pdf

--- src/main/java/com/example/pdf/controller/PDFController.java
+++ src/main/java/com/example/pdf/controller/PDFController.java
@@ -1,5 +1,8 @@
 package com.example.pdf.controller;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.qrcode.ByteArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.InputStreamResource;
@@ -7,12 +10,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.print.Doc;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
@@ -22,11 +23,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
-@RequestMapping("downloads")
+@RequestMapping("/downloads")
 public class PDFController {
     private static final String EXTERNAL_FILE_PATH = "C:/Users/AprajitaBajpai/Downloads/";
 
-
+//method written for downloading file
     @RequestMapping("/file/{fileName:.+}")
     public ResponseEntity<ByteArrayResource> downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
                                                     @PathVariable("fileName") String fileName) throws IOException {
@@ -48,6 +49,33 @@ public class PDFController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .body(resource);
     }
+//method written for generating a file and converting that to pdf
+    @RequestMapping(value="/pdfFile")
+    public ResponseEntity<ByteArrayResource> generatePdf() throws DocumentException, FileNotFoundException {
+        Document pdfDoc = new Document(PageSize.A4);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PdfWriter.getInstance(pdfDoc, out);
+
+        pdfDoc.open();
+
+        Font myfont = new Font();
+        myfont.setStyle(Font.NORMAL);
+        myfont.setSize(11);
+        pdfDoc.add(new Paragraph("\n"));
+        pdfDoc.addTitle(" Disclaimer");
+        Paragraph para=new Paragraph("Loans are subjected to offence",myfont);
+        para.setAlignment(Element.ALIGN_JUSTIFIED);
+        pdfDoc.add(para);
+        pdfDoc.close();
+
+
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "loan.pdf" + "\"")
+                .body(new ByteArrayResource(out.toByteArray()));
+
+}
 
 
 }
